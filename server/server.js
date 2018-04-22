c4fe715c4a3c57469a9050dcf119fe5148a15633
Sikeat <kobsikeat@outlook.com>
2018-04-22 10:37:38 +0700

    Get Individual Request to MongoDB

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {UserSchema} = require('./models/user');
@@ -31,6 +32,28 @@ app.get('/todos', (req, res) => {
 	});
 });
 
+// Individual Request with Paramerter
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	
+	// Valid id with isValid
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+
+	TodoSchema.findById(id).then((todo) => {
+		if(!todo){
+			return res.status(404).send();
+		}
+		res.send({todo});
+	}).catch((e) => {
+		return res.status(404).send();
+	});
+});
+
+
+
+// Starting the Server
 app.listen(3000, () => {
 	console.log('Server started on port 3000');
 })
